0132d2929d548f1b2c5dc4e2b05a65c2c5355f12
nambur <nambur@wisc.edu>
2013-11-07 15:28:01 -0600

    last vga commit and this commit must go together

--- source/vga_controller/display_plane_tb.v
+++ source/vga_controller/display_plane_tb.v
@@ -4,17 +4,25 @@
 module display_plane_tb();
     
 reg clk, rst, fifo_empty, fifo_full;
-reg[23:0] in_pixel;
+reg[18:0] Waddr;
 
-wire fifo_wr;
-wire[12:0] rom_addr;
-wire[23:0] out_pixel;
+wire fifo_wr, ready;
+wire[19:0] ram_Raddr, ram_Waddr;
 
-display_pane_2 disp(clk, rst, fifo_full, in_pixel, fifo_wr, rom_addr, out_pixel);
+display_plane disp(clk, rst, fifo_empty, fifo_full, Waddr, ram_Raddr, ram_Waddr, fifo_wr, ready);
 
 initial begin
     clk = 1'b0;
     forever #10 clk = ~clk;
+end
+
+always @(posedge clk) begin
+    if (rst)
+        Waddr <= 19'd0;
+    else if (ready)
+        Waddr <= (Waddr + 1) % 19'h4B000;
+    else 
+        Waddr <= Waddr;
 end
 
 initial begin
@@ -23,19 +31,30 @@ initial begin
     fifo_empty = 1'b1;
     #100;
     rst = 1'b0;
-    #10000;
+    #100;
+
+    while (ready == 1'b1) begin
+        #100;
+    end
+
+    #1000;
     fifo_empty = 1'b0;
     fifo_full = 1'b1;
     #1000;
     fifo_empty = 1'b1;
     fifo_full = 1'b0;
-    #10000;
+    #1000;
     fifo_empty = 1'b0;
     fifo_full = 1'b1;
     #1000;
     fifo_empty = 1'b1;
     fifo_full = 1'b0;
-    #1000000;
+    #1000;
+
+    while (ready == 1'b0) begin
+        #100;
+    end
+    #1000;
     $stop;
 end
 
