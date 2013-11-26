25a279273298d795efddd10f8e1ff99ca7d869de
John E Cabaj <cabaj@tux-105.cae.wisc.edu>
2013-11-26 16:14:14 -0600

    More Ball.v changes

--- source/Graphics/Ball.v
+++ source/Graphics/Ball.v
@@ -4,6 +4,8 @@ module Ball(output[2:0] color, input [15:0] x_loc, y_loc, z_loc, pixel_x, pixel_
     wire [13:0] addr;
     wire [5:0] x_bound, y_bound;
     wire [3:0] zone;
+    wire [2:0] rom_data, data_0, data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8, data_9;
+    wire active;
     
     localparam STAGE_0 = 8'd69;
     localparam STAGE_1 = 7'd59;
@@ -17,16 +19,16 @@ module Ball(output[2:0] color, input [15:0] x_loc, y_loc, z_loc, pixel_x, pixel_
     localparam STAGE_9 = 5'd15;
     
     /*
-    Ball_ROM stage_0(.clka(clk), .addra(addr), .douta(data));
-    Ball_ROM stage_1(.clka(clk), .addra(addr), .douta(data));
-    Ball_ROM stage_2(.clka(clk), .addra(addr), .douta(data));
-    Ball_ROM stage_3(.clka(clk), .addra(addr), .douta(data));
-    Ball_ROM stage_4(.clka(clk), .addra(addr), .douta(data));
-    Ball_ROM stage_5(.clka(clk), .addra(addr), .douta(data));
-    Ball_ROM stage_6(.clka(clk), .addra(addr), .douta(data));
-    Ball_ROM stage_7(.clka(clk), .addra(addr), .douta(data));
-    Ball_ROM stage_8(.clka(clk), .addra(addr), .douta(data));
-    Ball_ROM stage_9(.clka(clk), .addra(addr), .douta(data));
+    Ball_ROM rom_0(.clka(clk), .addra(addr), .douta(data_0));
+    Ball_ROM rom_1(.clka(clk), .addra(addr), .douta(data_1));
+    Ball_ROM rom_2(.clka(clk), .addra(addr), .douta(data_2));
+    Ball_ROM rom_3(.clka(clk), .addra(addr), .douta(data_3));
+    Ball_ROM rom_4(.clka(clk), .addra(addr), .douta(data_4));
+    Ball_ROM rom_5(.clka(clk), .addra(addr), .douta(data_5));
+    Ball_ROM rom_6(.clka(clk), .addra(addr), .douta(data_6));
+    Ball_ROM rom_7(.clka(clk), .addra(addr), .douta(data_7));
+    Ball_ROM rom_8(.clka(clk), .addra(addr), .douta(data_8));
+    Ball_ROM rom_9(.clka(clk), .addra(addr), .douta(data_9));
     */
     
     assign zone = (z_loc <= 7'd99) ? 4'd0 :
@@ -59,7 +61,20 @@ module Ball(output[2:0] color, input [15:0] x_loc, y_loc, z_loc, pixel_x, pixel_
                      (zone == 4'd7) ? y_loc + STAGE_7 :
                      (zone == 4'd8) ? y_loc + STAGE_8 : y_loc + STAGE_9;
      
-    assign addr =               
-   
-	
+    assign addr = ((pixel_x - (x_loc - 16'd1)) - (pixel_y - y_loc));
+    
+    assign active = ((pixel_x >= x_loc) && (pixel_x <= x_bound)) && ((pixel_y >= y_loc) && (pixel_y <= y_bound)) ? 1'b1 : 1'b0;         
+      
+	 assign rom_data = (zone == 4'd0) ? data_0 :
+                      (zone == 4'd1) ? data_1 :
+                      (zone == 4'd2) ? data_2 :
+                      (zone == 4'd3) ? data_3 :
+                      (zone == 4'd4) ? data_4 :
+                      (zone == 4'd5) ? data_5 :
+                      (zone == 4'd6) ? data_6 :
+                      (zone == 4'd7) ? data_7 :
+                      (zone == 4'd8) ? data_8 : data_9;
+                      
+    assign color = rom_data;
+	 
 endmodule
