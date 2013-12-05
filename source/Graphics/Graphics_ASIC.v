ae8ca4e6f9e247aa8484419ee2f8344310442cd9
Edward Langhus Tashjian <etashjia@tux-32.cae.wisc.edu>
2013-12-05 12:40:29 -0600

    Something bad happened. Reverting old graphics ASIC to base copy

--- source/Graphics/Graphics_ASIC.v
+++ source/Graphics/Graphics_ASIC.v
@@ -1,144 +1,49 @@
 
 module Graphics_ASIC(
-    input clk,
+   input clk,
 	input rst,
-	input chipselect,
-    input read,
+	input[3:0] chipselect,
 	input[15:0] databus,
 	input[3:0] data_address,
 	input VGA_ready,
-    output[23:0] color,
-    output[18:0] pixel_address
-    );
-
-	reg[15:0] paddle_1_x, paddle_1_x_buffer;
-	reg[15:0] paddle_1_y, paddle_1_y_buffer;
-	reg[15:0] paddle_2_x, paddle_2_x_buffer;
-	reg[15:0] paddle_2_y, paddle_2_y_buffer;
-	reg[15:0] ball_x, ball_x_buffer;
-	reg[15:0] ball_y, ball_y_buffer;
-	reg[15:0] ball_z, ball_z_buffer;
-	reg[15:0] player_1_score, player_1_score_buffer;
-	reg[15:0] player_2_score, player_2_score_buffer;
-	reg[15:0] game_state, game_state_buffer;
-
-    wire[15:0] next_paddle_1_x, next_paddle_1_y;
-    wire[15:0] next_paddle_2_x, next_paddle_2_y;
-    wire[15:0] next_ball_x, next_ball_y, next_ball_z;
-    wire[15:0] next_player_1_score, next_player_1_score;
-
+   output[23:0] color,
+   output[18:0] pixel_address
+   );
+	wire [15:0] paddle_1_x, next_pad1_x;
+	reg[15:0] paddle_1_x_buffer;
+	wire [15:0] paddle_1_y, paddle_1_y_buffer;
+	wire [15:0] paddle_2_x, paddle_2_x_buffer;
+	wire [15:0] paddle_2_y, paddle_2_y_buffer;
+	reg [15:0] ball_x, ball_x_buffer;
+	reg [15:0] ball_y, ball_y_buffer;
+	reg [15:0] ball_z, ball_z_buffer;
+	wire [15:0] player_1_score, player_1_score_buffer;
+	wire [15:0] player_2_score, player_2_score_buffer;
+	reg [15:0] game_state, game_state_buffer;
 	wire[15:0] pixel_x, pixel_y;
 	wire[23:0] paddle_1_color, paddle_2_color, ball_color,  frame_score_color;
-
-
-	// logic for next values of databus
-    always @( * ) begin
-        next_paddle_1_x = paddle_1_x;
-        next_paddle_1_y = paddle_1_y;
-        next_paddle_2_x = paddle_2_x;
-        next_paddle_2_y = paddle_2_y;
-        next_ball_x = ball_x;
-        next_ball_y = ball_y;
-        next_ball_z = ball_z;
-        next_player_1_score = player_1_score;
-        next_player_2_score = player_2_score;
-        next_game_state = game_state;
-        next_databus = 16'hzzzz;
-
-        if (rst) begin
-            next_paddle_1_x = 16'd320;
-            next_paddle_1_y = 16'd240;
-            next_paddle_2_x = 16'd320;
-            next_paddle_2_y = 16'd240;
-            next_ball_x = 16'd320;
-            next_ball_y = 16'd240;
-            next_ball_z = 16'd0;
-            next_player_1_score = 16'd0;
-            next_player_2_score = 16'd0;
-            next_game_state = 16'd0;
-        end
-        else if (chipselect) begin
-            if (read) begin
-                case (data_address) begin
-                    4'h0: next_databus = paddle_1_x;
-                    4'h1: next_databus = paddle_1_y;
-                    4'h2: next_databus = paddle_2_x;
-                    4'h3: next_databus = paddle_2_y;
-                    4'h4: next_databus = ball_x;
-                    4'h5: next_databus = ball_y;
-                    4'h6: next_databus = ball_z;
-                    4'h7: next_databus = player_1_score;
-                    4'h8: next_databus = player_2_score;
-                    4'h9: next_databus = game_state;
-                end
-            end
-            else begin
-                case (data_address) begin
-                    4'h0: next_paddle_1_x = databus;
-                    4'h1: next_paddle_1_y = databus;
-                    4'h2: next_paddle_2_x = databus;
-                    4'h3: next_paddle_2_y = databus;
-                    4'h4: next_ball_x = databus;
-                    4'h5: next_ball_y = databus;
-                    4'h6: next_ball_z = databus;
-                    4'h7: next_player_1_score = databus;
-                    4'h8: next_player_2_score = databus;
-                    4'h9: next_game_state = databus;
-                end
-            end
-        end
-    end
-
-    // buffer registers from databus
-    always @(posedge clk) begin
-        if (rst) begin
-            paddle_1_x <= 16'd320;
-            paddle_1_y <= 16'd240;
-            paddle_2_x <= 16'd320;
-            paddle_2_y <= 16'd240;
-            ball_x <= 16'd320;
-            ball_y <= 16'd240;
-            ball_z <= 16'd0;
-            player_1_score <= 16'd0;
-            player_2_score <= 16'd0;
-            game_state <= 16'd0;
-        end
-        else begin
-            paddle_1_x <= next_paddle_1_x;
-            paddle_1_y <= next_paddle_1_y;
-            paddle_2_x <= next_paddle_2_x;
-            paddle_2_y <= next_paddle_2_y;
-            ball_x <= next_ball_x;
-            ball_y <= next_ball_y;
-            ball_z <= next_ball_z;
-            player_1_score <= next_player_1_score;
-            player_2_score <= next_player_2_score;
-            game_state <= next_game_state;
-        end
-    end
 	
-    // buffer the registers on begining of a frame
+	assign player_1_score_buffer = 16'd1;
+	assign player_2_score_buffer = 16'd2;
+	
+	assign paddle_2_x_buffer = 16'd350;
+	assign paddle_2_y_buffer = 16'd250;
+	
+	assign next_pad1_x = (VGA_ready && pixel_address == 19'h4AFFF) ?
+									(paddle_1_x_buffer <= 16'd400) ?
+										paddle_1_x_buffer + 16'd1
+									: 16'd100
+								:paddle_1_x_buffer;
+								
+	assign paddle_1_y_buffer = 16'd200;
+	
 	always @(posedge clk) begin
 		if (rst) begin
-			paddle_1_x_buffer <= paddle_1_x;
-			paddle_1_y_buffer <= paddle_1_y;
-			paddle_2_x_buffer <= paddle_2_x;
-			paddle_2_y_buffer <= paddle_2_y;
-            ball_x_buffer <= ball_x;
-            ball_y_buffer <= ball_y;
-            ball_z_buffer <= ball_z;
-            game_state_buffer <= game_state;
+			paddle_1_x_buffer <= 16'd100;
 		end
-		else if (pixel_address == 19'h4AFFF && VGA_ready) begin
-			paddle_1_x_buffer <= paddle_1_x;
-			paddle_1_y_buffer <= paddle_1_y;
-			paddle_2_x_buffer <= paddle_2_x;
-			paddle_2_y_buffer <= paddle_2_y;
-            ball_x_buffer <= ball_x;
-            ball_y_buffer <= ball_y;
-            ball_z_buffer <= ball_z;
-            game_state_buffer <= game_state;
-        end
+		else begin
+			paddle_1_x_buffer <= next_pad1_x;
+		end
 	end
 
 	Paddle_1 paddle_1(.clk(clk),
@@ -156,7 +61,7 @@ module Graphics_ASIC(
 					.pixel_x(pixel_x),
 					.pixel_y(pixel_y),
 					.color(paddle_2_color));
-					
+	/*				
 	Ball ball(.clk(clk),
 				.rst(rst),
 				.x_loc(ball_x_buffer),
@@ -165,6 +70,8 @@ module Graphics_ASIC(
 				.pixel_x(pixel_x),
 				.pixel_y(pixel_y),
 				.color(ball_color));
+	*/
+	assign ball_color = 0;
 
 	Frame_Score frame_score(.clk(clk),
 							.rst(rst),
