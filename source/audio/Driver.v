efb50e6ee762053c75fba1d1276be2308ad9af3b
Edward Langhus Tashjian <etashjia@tux-32.cae.wisc.edu>
2013-12-05 23:06:10 -0600

    Updated audio. No longer a status read, but subsequent writes will interupt audio playback

--- source/audio/Driver.v
+++ source/audio/Driver.v
@@ -3,34 +3,56 @@ module Driver(
 	input rst,
 	output cs,
 	output rw,
-	inout[15:0] data
+	inout[15:0] data,
+	output LED_0
     );
 	 
 	 parameter IDLE = 2'd0;
-	 parameter SEND = 2'd1;
-	 parameter DONE = 2'd3;
+	 parameter SEND = 2'd2;
+	 parameter INTERUPT = 2'd3;
+	 parameter DONE = 1'd1;
 
 	 reg[1:0] state, next_state;
 	 
-	 assign cs = 1'b1;
+	 assign cs = (state == IDLE) || (state == SEND);
 	 assign rw = ~(state == SEND);
 	 assign data = (state == SEND) ? 16'h8008 : 16'dz;
+	 
+	 reg[26:0] wait_cnt;
+	 reg[23:0] int_cnt;
+	 reg interupted;
+	 
+	 assign LED_0 = ~(state == INTERUPT);
 	 
 	 always@(posedge clk) begin
 		if(rst) begin
 			state <= IDLE;
+			wait_cnt <= 1;
+			int_cnt <= 1;
+			interupted <= 0;
+			
 		end
 		else begin 
 			state <= next_state;
+			wait_cnt <= wait_cnt + 1;
+			if(state == INTERUPT) begin 
+				int_cnt <= int_cnt + 1;
+				interupted <= 1;
+			end else int_cnt <= 1;
 		end
 	 end
 	 
 	 always@(*) begin
 		case(state)
-			IDLE: if(data) next_state = SEND;
-					else next_state = IDLE;
-			SEND: next_state = DONE;
+			IDLE: if(wait_cnt == 0) next_state = SEND;
+					else next_state = IDLE;		
+					
+			SEND: if(interupted) next_state = DONE;
+					else next_state = INTERUPT;
 			
+			INTERUPT: if(int_cnt == 0) next_state = SEND;
+			          else next_state = INTERUPT;
+						 
 			DONE: next_state = DONE;
 		endcase
 	end
--- source/audio/top_level.v
+++ source/audio/top_level.v
@@ -25,14 +25,22 @@ module top_level(
 			output SDATA_OUT, 
 			output SYNC, 
 			output BEEP_TONE_IN, 
-			output AUDIO_RESET_Z
+			output AUDIO_RESET_Z,
+			output LED_0,
+			output LED_1,
+			output LED_2,
+			output LED_3
     );		
 	
 	 assign BEEP_TONE_IN = 1'b0;
     
+	 assign LED_1 = 1'd1;
+	 assign LED_2 = 1'd1;
+	 assign LED_3 = 1'd1;
+	 
 	 wire cs, rw;
 	 wire[15:0] data;
-    Driver d(.clk(clk), .rst(rst), .cs(cs), .rw(rw), .data(data));
+    Driver d(.clk(clk), .rst(rst), .cs(cs), .rw(rw), .data(data), .LED_0(LED_0));
     Audio_Controller ac(.clk(clk), .rst(rst), .cs(cs), .rw(rw), .data(data), .BIT_CLK(BIT_CLK), .SDATA_OUT(SDATA_OUT), .SYNC(SYNC), .AUDIO_RESET_Z(AUDIO_RESET_Z));
 						  
 endmodule
