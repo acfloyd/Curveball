607d293a39e8994a871f1caed8c2b98d8bae30f5
Edward Langhus Tashjian <etashjia@tux-49.cae.wisc.edu>
2013-12-08 18:12:07 -0600

    source/Graphics/Frame_Score.v

--- source/top.v
+++ source/top.v
@@ -19,22 +19,8 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module top(input clk_100mhz,
-                          input rst,
-                          output blank,
-                          output comp_sync,
-                          output hsync,
-                          output vsync,
-                          output[7:0] pixel_r,
-                          output[7:0] pixel_g,
-                          output[7:0] pixel_b,
-                          output vgaclk,
-                          inout MOUSE_CLOCK,
-                          inout MOUSE_DATA,
-                          input BIT_CLK, 
-                          output SDATA_OUT, 
-                          output SYNC, 
-                          output AUDIO_RESET_Z
 			  input rst,
+			  input up,
 			  output blank,
 			  output comp_sync,
 			  output hsync,
@@ -42,90 +28,44 @@ module top(input clk_100mhz,
 			  output[7:0] pixel_r,
 			  output[7:0] pixel_g,
 			  output[7:0] pixel_b,
-			  output vgaclk,
-			  inout MOUSE_CLOCK,
-			  inout MOUSE_DATA,
-			  input BIT_CLK, 
-			  output SDATA_OUT, 
-			  output SYNC, 
-			  output AUDIO_RESET_Z
+			  output vgaclk
     );
-        
-        wire graphics_VGA_ready;
-    wire locked_dcm, cpu_locked_dcm, clkin_ibufg_out;
-    wire clk_100mhz_buf, clk_100mhz_buf2;
-	wire[23:0] graphics_color;
-	wire cpuclk;
-	wire ack;
-	wire Read, Write, CS_RAM, CS_Audio, CS_Graphics, CS_Spart, CS_PS2;
-	wire [15:0] Addr, WriteData, DataToCPU, DataBus, Instruct, NextPC;
+	
+	wire graphics_chipselect;
+	wire[15:0] graphics_databus;
+	wire[3:0] graphics_data_address;
+	wire graphics_VGA_ready;
+    wire locked_dcm;
+    wire clk_100mhz_buf;
+	wire[24:0] graphics_color;
+	wire[18:0] graphics_pixel_address;
+	
+	
 
-	proc PROC(.clk(cpuclk), 
-			  .rst(rst | ~cpu_locked_dcm), 
-			  .WriteMem(Write), 
-			  .ReadMem(Read), 
-			  .ExternalAddr(Addr),
-			  .ExternalWriteData(WriteData), 
-			  .ExternalReadData(DataToCPU), 
-			  .Instruct(Instruct), 
-			  .NextPC(NextPC));
-
-	External_Mem MEM(.Addr(Addr[15:12]), 
-					 .WriteData(WriteData), 
-					 .Read(Read), 
-					 .Write(Write),
-					 .DataToCPU(DataToCPU), 
-					 .DataBus(DataBus), 
-					 .CS_RAM(CS_RAM), 
-					 .CS_Audio(CS_Audio), 
-					 .CS_Graphics(CS_Graphics), 
-					 .CS_Spart(CS_Spart),
-					 .CS_PS2(CS_PS2));
-
-
-    Graphics_ASIC graphics(.clk(cpuclk),
-					.rst(rst | ~cpu_locked_dcm),
-					.read(Read),
-					.chipselect(CS_Graphics),
-					.databus(DataBus),
-					.data_address(Addr[3:0]),
+    Graphics_ASIC graphics(.clk(clk_100mhz_buf),
+					.rst(rst),
+					.chipselect(graphics_chipselect),
+					.databus(graphics_databus),
+					.data_address(graphics_data_address),
 					.VGA_ready(graphics_VGA_ready),
-					.color(graphics_color));
-					
-	ps2_mouse mouse(.r_ack(ack),
-						 .databus(DataBus), 
-						 .MOUSE_CLOCK(MOUSE_CLOCK), 
-						 .MOUSE_DATA(MOUSE_DATA), 
-						 .addr(Addr[1:0]), 
-						 .clk(cpuclk), 
-						 .rst(rst), 
-						 .io_cs(CS_PS2), 
-						 .read(Read));
-
-	Audio_Controller ac(.clk(cpuclk),
-						.rst(rst | ~cpu_locked_dcm), 
-						.cs(CS_Audio), 
-						.data(DataBus), 
-						.BIT_CLK(BIT_CLK), 
-						.SDATA_OUT(SDATA_OUT), 
-						.SYNC(SYNC), 
-						.AUDIO_RESET_Z(AUDIO_RESET_Z));
+					.color(graphics_color),
+					.pixel_address(graphics_pixel_address));
 	
 	
-	vga_controller vga(.clk_100mhz_buf(cpuclk),
+	vga_controller vga(.clk_100mhz_buf(clk_100mhz_buf),
 							 .rst(rst),
 							 .Wdata(graphics_color),
 							 .vgaclk(vgaclk),
                              .locked_dcm(locked_dcm),
-                                                         .state(graphics_VGA_ready),
-                                                         .blank(blank),
-                                                         .comp_sync(comp_sync),
-                                                         .hsync(hsync),
-                                                         .vsync(vsync),
-                                                         .pixel_r(pixel_r),
-                                                         .pixel_g(pixel_g),
-                                                         .pixel_b(pixel_b));
-                                                         
-        vga_clk vga_clk_gen1(clk_100mhz, rst, vgaclk, clk_100mhz_buf, locked_dcm);
-        cpu_clk cpu_clk_gen1(clk_100mhz, rst, cpuclk, clk_100mhz_buf2, cpu_locked_dcm);
+							 .state(graphics_VGA_ready),
+							 .blank(blank),
+							 .comp_sync(comp_sync),
+							 .hsync(hsync),
+							 .vsync(vsync),
+							 .pixel_r(pixel_r),
+							 .pixel_g(pixel_g),
+							 .pixel_b(pixel_b));
+							 
+	vga_clk vga_clk_gen1(clk_100mhz, rst, vgaclk, clkin_ibufg_out, clk_100mhz_buf, locked_dcm);
 endmodule
+
