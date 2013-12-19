4f12ac1570855eaaeef5f1fc8be7b294e63674d7
Addison Floyd <thefloydage@gmail.com>
2013-12-19 13:17:58 -0600

    Update assembler.pl
    Final Paper

--- tools/assembler.pl
+++ tools/assembler.pl
@@ -3,55 +3,54 @@
 open (myfile, '>>game_code.bin');
 
 %MCODE = (	ADDI	=> '00000',
-				SUBI	=> '00001',
-				MULTI	=> '00010',
-				DIVI	=> '00011',
-				ANDI	=> '00100',
-				ORI	=> '00101',
-				XORI	=> '00110',
-				ROLI	=> '01000',
-				SLLI	=> '01001',
-				SRLI	=> '01010',
-				SRAI	=> '01011',
-				LBI	=> '01100',
-				SLBI	=> '01101',
-				STI	=> '01110',
-				LDI	=> '01111',
-				ADD	=> '10000',
-				SUB	=> '10000',
-				MULT	=> '10000',
-				DIV	=> '10000',
-				AND	=> '10001',
-				OR		=> '10001',
-				XOR	=> '10001',
-				NOT	=> '10001',
-				ROL	=> '10010',
-				SLL	=> '10010',
-				SRL	=> '10010',
-				SRA	=> '10010',
-				SEQ	=> '10011',
-				SLT	=> '10011',
-				SLE	=> '10011',
-				SCO	=> '10011',
-				BEQZ	=> '10100',
-				BNEZ	=> '10101',
-				BLTZ	=> '10110',
-				BLEZ	=> '10111',
-				J		=> '11000',
-				JR		=> '11001',
-				JAL	=> '11010',
-				JALR	=> '11011',
-				HALT	=> '1110000000000000',
-				NOP	=> '1110100000000000',
-				ST		=> '11110',
-				LD		=> '11111');
+			SUBI	=> '00001',
+			MULTI	=> '00010',
+			DIVI	=> '00011',
+			ANDI	=> '00100',
+			ORI		=> '00101',
+			XORI	=> '00110',
+			ROLI	=> '01000',
+			SLLI	=> '01001',
+			SRLI	=> '01010',
+			SRAI	=> '01011',
+			LBI		=> '01100',
+			SLBI	=> '01101',
+			STI		=> '01110',
+			LDI		=> '01111',
+			ADD		=> '10000',
+			SUB		=> '10000',
+			MULT	=> '10000',
+			DIV		=> '10000',
+			AND		=> '10001',
+			OR		=> '10001',
+			XOR		=> '10001',
+			NOT		=> '10001',
+			ROL		=> '10010',
+			SLL		=> '10010',
+			SRL		=> '10010',
+			SRA		=> '10010',
+			SEQ		=> '10011',
+			SLT		=> '10011',
+			SLE		=> '10011',
+			SCO		=> '10011',
+			BEQZ	=> '10100',
+			BNEZ	=> '10101',
+			BLTZ	=> '10110',
+			BLEZ	=> '10111',
+			J		=> '11000',
+			JR		=> '11001',
+			JAL		=> '11010',
+			JALR	=> '11011',
+			HALT	=> '1110000000000000',
+			NOP		=> '1110100000000000',
+			ST		=> '11110',
+			LD		=> '11111');
 
 $addr=0;
 while(<>){						# go through every line in source file 
-	#push(@source,$_);
 	# pre-process
 	if(/(;.+)/){				# comments
-		s/;.+//;					# remove comment
+		s/;.+//;				# remove comment
 	}
 	if(/(\w+):/){				# word followed by a colon, meaning label
 		$label{$1}=$addr;
@@ -69,33 +68,24 @@ print"**** MACROS ****\n";
 foreach $1 (keys(%macro)){
 	printf"%s = %s\n",$1,$macro{$1};
 	foreach (@source){
-		#printf"%s",$_;
-		#printf"%s\n",$1;
 		s/$1/$macro{$1}/;			# replace macro label with value
-		#printf"%s",$_;
 	}
 }
 print"\n**** LABEL LIST ****\n";
 foreach $1 (keys(%label)){
-	#printf "%s",$1;
 	printf "%-10s%03d\n",$1,$label{$1};
 	$addr=0;
 	foreach (@source){
 		$addr++;
-		#printf"s: %s",$_;
-		#printf"%s\n",$1;
 		if($_ =~ /JR|LBI|SLBI/){
             $tstH = join "", $1, "_HIGH";
             $tstL = join "", $1, "_LOW";
-            #printf"tstH: %s, tstL: %s\n",$tstH, $tstL;
             if ($_ =~ /$tstH/) {
                 $val = $label{$1} >> 8;
-                #printf"tstH val: %s\n",$val;
                 s/$tstH/#$val/;			# replace label with address
             }
             elsif ($_ =~ /$tstL/) {
                 $val = $label{$1} & (255);
-                #printf"tstL val: %s\n",$val;
                 s/$tstL/#$val/;			# replace label with address
             }
             else {
@@ -104,10 +94,8 @@ foreach $1 (keys(%label)){
 		}
 		else {
 			$offset = $label{$1}-$addr;
-			#printf"Label: %s, Address: %s, Offset: %s\n",$label{$1},$addr,$offset;
 			s/$1/#$offset/;	# replace label with pc offset
 		}
-		#printf"%s",$_;
 	}
 }
 
@@ -115,7 +103,6 @@ $addr=0;
 print "\n**** MACHINE PROGRAM ****\n";
 foreach (@source){
 	$line = $_;
-	#s/\w+://;
 	if(/(ADDI|SUBI|MULTI|DIVI|ANDI|ORI|XORI|ROLI|SLLI|SRLI|SRAI)\s+R(\d),\s+R(\d),\s+#(-?\d+)/){
 		# finds ADDI Rd, Rs, #immediate, etc.
 		if($4 < 0){		# negative immediate
@@ -213,5 +200,3 @@ foreach (@source){
 		print "\t\t$line";
 	}
 }
-
-# code modified from http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=2&ved=0CDQQFjAB&url=http%3A%2F%2Fwww.cs.hiroshima-u.ac.jp%2F~nakano%2Fwiki%2Fwiki.cgi%3Faction%3DATTACH%26page%3DCourse%2BMaterials%26file%3Demb5.pdf&ei=JqR6Us-4LKKGyQHzg4DgDA&usg=AFQjCNHGQrfRnJ6dLqdD7QuH7fIAoFCU5g&sig2=wCGjvORTyfGyGsF46dvG9g&bvm=bv.55980276,d.aWc&cad=rja
