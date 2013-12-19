7eb8a49735a9399f5a739e478af6a4422e7821ea
ngwilliams <ngwilliams@wisc.edu>
2013-12-19 13:31:36 -0600

    Update Decode.v
    Added comments

--- source/cpu/Decode.v
+++ source/cpu/Decode.v
@@ -19,41 +19,52 @@ module Decode(clk, rst, Stall, Instruct, DataIn, WrEn, SignExtSel, ZeroExtend8, 
 	wire BranchFlag, DataZero, Flag, JumpOrBranchFlag;
 	wire [2:0] RtReadAddr;
 
+	//Sign extend immediate values
 	assign SignExt5 = {{11{Instruct[4]}}, Instruct[4:0]};
 	assign SignExt8 = {{8{Instruct[7]}}, Instruct[7:0]};
 	assign SignExt11 = {{5{Instruct[10]}}, Instruct[10:0]};
 	assign ZeroExtOut = (ZeroExtend8) ? {{8{1'b0}}, Instruct[7:0]} : {{12{1'b0}}, Instruct[3:0]};
-
+	
+	//Selects the appropriate sign extended value
 	assign SignExt = (SignExtSel == 2'b00) ? SignExt5 : 
 					 (SignExtSel == 2'b01) ? SignExt8 :
 					 (SignExtSel == 2'b10) ? SignExt11 :
 					 (SignExtSel == 2'b11) ? ZeroExtOut : 16'dz;
 
 	wire [15:0] RegOut1Wire, RegOut2Wire;
+	
+	//Rt is in a different location in different instructions, depending on the instruction, determine the correct value of Rt
 	assign RtReadAddr = (StoreImmed) ? Instruct[10:8] : Instruct[7:5];
 	//Register Mem
 	Reg_Mem Reg(.clk(clk), .rst(rst), .DataIn(DataIn), .AddrS(Instruct[10:8]),
 	            .AddrT(RtReadAddr), .WrSel(WrRegAddr), .WrRegEn(WrEn), .Rs(RegOut1Wire), .Rt(RegOut2Wire));
 
+	//Determines Data to be written into a register. Can be forwarded or from Write Back
 	assign WriteDataIn = (WrEn & (WrRegAddr == RtReadAddr)) ? DataIn : RegOut2Wire;
 
+	//Determines data coming from the registers. Can be forwarded data as well
 	assign RegOut1 = (WrEn & (WrRegAddr == Instruct[10:8])) ? DataIn : RegOut1Wire;
 	assign RegOut2 = (WrEn & (WrRegAddr == Instruct[7:5])) ? DataIn : RegOut2Wire; 
-
+	
+	//In the case of a JALR or JAL, writes the PC value into R7
 	assign DataOut1RegIn = (LoadR7) ? NextPC : DataOut1Temp;
-
+	
+	//Register value to determine the next PC value in case of a JR or JALR
 	assign JumpRegMuxOut = BranchOrJumpRegForwardRs ? DataOut1Temp : RegOut1;
 
+	//Determines if the value of Rt is immediate or from Register 2
 	assign DataOut2RegIn = (DataOut2Sel) ? SignExt : RegOut2;
 
+	//Data forwarding
 	assign DataOut1Temp = (BJForwardSel == 2'b00) ? ALUForward: 
 					  (BJForwardSel == 2'b01) ? MemData2:
 					  (BJForwardSel == 2'b10) ? WriteBackData:
 					  RegOut1;
-
+	//Logic to determine next PC
 	assign PCAddIn1 = (NextPCSel) ? NextPC : JumpRegMuxOut;
 	assign PCAddIn2 = (BranchImmedSel) ? SignExt11 : SignExt8;
-
+	
+	//Flag logic (flags used in Branches)
 	assign DataZero = ~|DataOut1RegIn;
 	assign Flag = (SetFlag == 2'b00) ? DataZero : //BEQZ
                   (SetFlag == 2'b01) ? ~DataZero : //BNEZ
