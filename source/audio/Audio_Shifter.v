f806356bd4fc00739bbb7138ab76bb2ed0ecdc99
etashjian <luekwa@gmail.com>
2013-12-19 14:23:44 -0600

    Update Audio_Shifter.v
    Added comments to Audio_Shifter.v

--- source/audio/Audio_Shifter.v
+++ source/audio/Audio_Shifter.v
@@ -1,21 +1,37 @@
-module Audio_Shifter(input clk, input rst, input load, input[75:0] data, 
-							output ready, output SYNC, output SDATA_OUT);
+module Audio_Shifter(
+	input clk, // BIT_CLK
+	input rst, // global reset
+	input load, // load signal indicating input
+	input[75:0] data, // data to be shifted
+	output ready, // indicates shifter is ready for new cmd
+	output SYNC, // SYNC FPGA pin
+	output SDATA_OUT // SDATA_OUT FPGA pin
+	);
 
-	parameter IDLE = 1'b0;
-	parameter SHIFTING = 1'b1;
+	// state parameters
+	parameter IDLE = 1'b0; // Idle state
+	parameter SHIFTING = 1'b1; // sending state
 
+	// state regs
 	reg state;
 	wire next_state;
+	
+	// counting values
 	reg[7:0] bit_counter;
 	wire[7:0] next_bit_counter;
+	
+	// shifting data
 	reg[76:0] shifter;
 	wire[76:0] next_shifter;
 
 	assign ready = (state == IDLE);
+	
+	// next state logic
 	assign next_state =  (state == IDLE && load) ? SHIFTING : 
-								(state == IDLE && !load) ? IDLE:
-								(state == SHIFTING && bit_counter == 8'd255) ? IDLE : SHIFTING;
-								
+			     (state == IDLE && !load) ? IDLE:
+			     (state == SHIFTING && bit_counter == 8'd255) ? IDLE : SHIFTING;
+				
+	// update counter and shift data							
 	assign next_bit_counter = (state == SHIFTING) ? bit_counter + 1 : 8'd0;
 	assign next_shifter = (state == IDLE) ? {1'b0, data} : {shifter[75:0], 1'b0};
 
