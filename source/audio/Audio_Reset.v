5658ffc9e82c3aefd4078f4d36cf3ebedaafd8a0
etashjian <luekwa@gmail.com>
2013-12-19 14:12:35 -0600

    Update Audio_Reset.v
    Added comments to Audio_Reset.v

--- source/audio/Audio_Reset.v
+++ source/audio/Audio_Reset.v
@@ -1,15 +1,25 @@
-module Audio_Reset(input clk, input rst, output AUDIO_RESET_Z, output audio_ready);
+// Module applies appropriate reset sequence to audio controller after rst goes low
 
-	parameter START = 2'd0;
-	parameter RESET = 2'd1;
-	parameter HOLD = 2'd2;
+module Audio_Reset(
+	input clk, // BIT_CLK 
+	input rst, // global reset
+	output AUDIO_RESET_Z, // AUDIO_RESET_Z FPGA pin
+	output audio_ready // ready signal to begin work on audio codec
+	);
+
+	parameter START = 2'd0; // start state, sets Audio_RESET_Z high ~.5 s 
+	parameter RESET = 2'd1; // reset state, sets Audio_RESET_Z low ~.5 s
+	parameter HOLD = 2'd2; // finisehd state, leaves AudioRESET_Z high
 	
+	// state and wait counters
 	reg[1:0] state, next_state;
 	reg[23:0] wait_count;
 	
-	assign AUDIO_RESET_Z = (state != RESET);
-	assign audio_ready = (state == HOLD);
 	
+	assign AUDIO_RESET_Z = (state != RESET); // set reset low in reset state, high otherwise
+	assign audio_ready = (state == HOLD); // assert ready once reset sequence finished
+	
+	// sequential logic update
 	always@(posedge clk) begin
 		if(rst) begin
 			state <= START;
@@ -20,6 +30,7 @@ module Audio_Reset(input clk, input rst, output AUDIO_RESET_Z, output audio_read
 		end
 	end
 	
+	// next_state logic
 	always@(*) begin
 		case(state)
 			START: if(wait_count == 0) next_state = RESET;
