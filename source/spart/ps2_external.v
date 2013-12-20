a0c5aaf858eb8b891237c43f78524619e5c5438f
John <cabaj@wisc.edu>
2013-12-19 22:49:21 -0600

    Update ps2_external.v
    Commented ps2_external

--- source/spart/ps2_external.v
+++ source/spart/ps2_external.v
@@ -1,16 +1,17 @@
-module ps2_external(clk, rst, cs, addr_in, DataBus, rxd);
-
-	input clk, rst, cs, rxd;
-	input[1:0] addr_in;
-	inout[15:0] DataBus;
+//module that wraps the functionality of the read and read_driver modules
+module ps2_external(
+	input clk, rst, cs, rxd;			//clk, rst, and chip select signals
+	input[1:0] addr_in;				//read address from read_driver	
+	inout[15:0] DataBus;				//input/output databus
+);
 	
-	reg Drive_DataBus;
-	
-	wire rda;
-	wire[7:0] data_in;
-	wire[15:0] spart_data;
-	reg [15:0] data_driven;
+	reg Drive_DataBus;				//control tristate databus
+	wire rda;					//receive data available
+	wire[7:0] data_in;				//data read from the receiver
+	wire[15:0] spart_data;				//data read from receiver
+	reg [15:0] data_driven;				//data driven onto databus
 
+	//databus logic
 	always @(posedge clk or posedge rst) begin
 		if (rst) begin
 			Drive_DataBus <= 1'b0;
@@ -26,22 +27,22 @@ module ps2_external(clk, rst, cs, addr_in, DataBus, rxd);
 		end
 	end
 
+	//write to or disconnect databus
 	assign DataBus = (Drive_DataBus) ? data_driven : 16'hzzzz;
 	
-	// Instantiate your driver here
-	read_driver driver1( .clk(clk),
-	                .rst(rst),
-						 .rda(rda),
-						 .data_in(data_in),
-						 .addr(addr_in),
-						 .data_out(spart_data)
-					 );
-					 
-	reader spart1( .clk(clk),
-                 .rst(rst),
-					  .rda(rda),
-					  .data_out(data_in),
-					  .rxd(rxd)
-					);
+	//instantiate read_driver module
+	read_driver driver1(.clk(clk),
+	                    .rst(rst),
+			    .rda(rda),
+			    .data_in(data_in),
+			    .addr(addr_in),
+			    .data_out(spart_data));
+				
+	//instantiating read module	 
+	reader spart1(.clk(clk),
+                      .rst(rst),
+		      .rda(rda),
+		      .data_out(data_in),
+		      .rxd(rxd));
 
 endmodule
