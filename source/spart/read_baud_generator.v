5ae7bf8b29e734ec84421b837fdfe329c9c659a6
John <cabaj@wisc.edu>
2013-12-19 23:07:51 -0600

    Update read_baud_generator.v

--- source/spart/read_baud_generator.v
+++ source/spart/read_baud_generator.v
@@ -1,27 +1,9 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 	UW-Madison ECE 554
-// Engineer: 	John Cabaj, Nate Williams, Paul McBride
-// 
-// Create Date:    September 15, 2013
-// Design Name: 	 SPART
-// Module Name:    baud_generator
-// Project Name: 		Mini-Project 1 - SPART
-// Target Devices: 	Xilinx Vertex II FPGA
-// Tool versions: 
-// Description: 		Controls sampling and baud rate signals
-//
-// Dependencies: 
-//
-// Revision: 		1.0
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module read_baud_generator(clk, rst, rxEnable);
-    
-    input rst, clk;				// inputs 
-    output reg rxEnable;			//outputs
+//generators receive signals at baud rate
+module read_baud_generator(
+    input rst, 						//system reset
+    input clk,						//system clock 
+    output reg rxEnable					//receive enable
+);	
     reg [15:0] counter;					// counter to decrement
     reg [3:0] txCount;					// count for baud rate signal
     reg txEnable;
