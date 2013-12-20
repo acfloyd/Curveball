aff66429de2b5e76b2954574aba5e08ec83500da
John <cabaj@wisc.edu>
2013-12-19 22:47:44 -0600

    Update ps2_internal.v

--- source/ps2/ps2_internal.v
+++ source/ps2/ps2_internal.v
@@ -9,7 +9,8 @@ module ps2_internal(
 	wire[15:0] ps2_mouse_data,			//data read from ps2_mouse
 	wire[1:0] ps2_mouse_addr,			//address to read from ps2_mouse
 	wire[7:0] data_out,				//data to be transmit out
-	wire tbr);					//transmit buffer ready
+	wire tbr					//transmit buffer ready
+);							
 	
 	//instantiate ps2_mouse
 	ps2_mouse mouse(.r_ack(ack),
