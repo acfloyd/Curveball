cbf3ef29268716a8caeee5ff80647cc35e0722c3
John <cabaj@wisc.edu>
2013-12-19 22:55:23 -0600

    Update write_baud_generator.v

--- source/spart/write_baud_generator.v
+++ source/spart/write_baud_generator.v
@@ -1,23 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 	UW-Madison ECE 554
-// Engineer: 	John Cabaj, Nate Williams, Paul McBride
-// 
-// Create Date:    September 15, 2013
-// Design Name: 	 SPART
-// Module Name:    baud_generator
-// Project Name: 		Mini-Project 1 - SPART
-// Target Devices: 	Xilinx Vertex II FPGA
-// Tool versions: 
-// Description: 		Controls sampling and baud rate signals
-//
-// Dependencies: 
-//
-// Revision: 		1.0
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+//generators transmit signals at baud rate
 module write_baud_generator(clk, rst, txEnable);
     
     input rst, clk;				// inputs 
